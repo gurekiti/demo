61ddd7e994a2dc34c27cd8ea74b4a4aca7206b0f
gurekiti <sakura04155@gmail.com>
2018-12-04 23:30:53 +0900

    コンパイルターゲットを追加

---
+++ src/test/java/com/example/demo/CircleTest.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+public class CircleTest {
+    @Test
+    @DisplayName("boolean")
+    void test(){
+        fail("circle error");
+    }
+}
